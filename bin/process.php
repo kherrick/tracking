d80ba0c936f74ef3c67cb57aa5ca8215cc0c0c28
Karl Herrick <me@karlherrick.com>
2013-10-20 18:54:24 -0400

    changing how the data is formatted

--- bin/process.php
+++ bin/process.php
@@ -1,11 +1,11 @@
 #!/usr/bin/php
 <?php
 
-//DT:4-15-2011_11.12@BATT:12,SMSRF:+15558675309,LOC:32.5241664,-73.1145664,LOCACC:49,LOCALT:165.3000030517578,LOCSPD:0.0,LOCTMS:1458423011,LOCN:32.5241664,-73.1145664,LOCNACC:101,LOCNTMS:1458423011,CELLID:GSM:12245.14455082,CELLSIG:4,CELLSRV:service
-
 class scriptHandler
 {
-    public $results = array();
+    public $fixture = array('DT:4-15-2011_11.12@BATT:12,SMSRF:+15558675309,LOC:32.5241664,-73.1145664,LOCACC:49,LOCALT:165.3000030517578,LOCSPD:0.0,LOCTMS:1458423011,LOCN:32.5241664,-73.1145664,LOCNACC:101,LOCNTMS:1458423011,CELLID:GSM:12245.14455082,CELLSIG:4,CELLSRV:service');
+
+    public $results  = array();
 
     /**
      * getter for results
@@ -27,9 +27,6 @@ class scriptHandler
         return file($filename);
     }
 
-    /**
-     * processes the lines of a file
-     */
     public function lineHandler($file)
     {
         foreach ($file as $line) {
@@ -63,7 +60,11 @@ class scriptHandler
         $at         = explode('@', $comma[0]);
         $underscore = explode('_', $at[0]);
         $colon      = explode(':', $underscore[0]);
-        $results    = $colon[1];
+        $date       = explode('-', $colon[1]);
+        $year       = $date[2];
+        $month      = $date[0];
+        $day        = $date[1];
+        $results    = $year . '-' . $month  . '-' . $day;
 
         return $results;
     }
@@ -73,7 +74,10 @@ class scriptHandler
         $comma      = explode(',', $line);
         $at         = explode('@', $comma[0]);
         $underscore = explode('_', $at[0]);
-        $results    = $underscore[1];
+        $time       = explode('.', $underscore[1]);
+        $hour       = $time[0];
+        $minute     = $time[1];
+        $results    = $hour . ':' . $minute;
 
         return $results;
     }
@@ -140,7 +144,7 @@ class scriptHandler
 
         // data is saved in meters per second
         // for miles per hour use:
-        // $results = $results * 2.2369363;
+        $results = $results * 2.2369363;
 
         return $results;
     }
@@ -218,9 +222,8 @@ class scriptHandler
 }
 
 $script      = new scriptHandler;
-$fileHandler = $script->fileHandler('../logs/2013-Oct-20_post_capture.log');
+$fileHandler = $script->fixture;
 $lineHandler = $script->lineHandler($fileHandler);
 $results     = $script->getResults();
 
-// echo json_encode($results);
 print_r($results);
